7947a258127cd46ec82a20ca0d90f2cd3b0b0223
ajaymahiwal <ajaymahiwal5@gmail.com>
2023-10-28 21:09:43 +0530

    Code

--- 1.9_DB_CRUD/app.js
+++ 1.9_DB_CRUD/app.js
@@ -36,11 +36,14 @@ app.get("/",(req,res)=>{
     res.send("<h1>It's Working.</h1>");
 });
 
+//Show All Chats
 app.get("/chats",async (req,res)=>{
     let chats = await Chat.find();
     res.render("chats.ejs",{chats});
 });
 
+
+//New Chat (Insert)
 app.get("/chats/new",(req,res)=>{
     res.render("newChat");
 });
@@ -62,12 +65,38 @@ app.post("/chats",(req,res)=>{
         console.log(err);
     });
 
-    let chats = Chat.find();
     res.redirect("/chats");
 });
 
 
+// Edit & Update Route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    // console.log(chat);
+    res.render("edit",{ chat });
+});
 
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let {msg:newMsg} = req.body;
+    // console.log(newMsg);
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id,
+        {msg:newMsg},
+        {runValidators:true, new:true});
+
+    // console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+//Delete Route
+app.delete("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findByIdAndDelete(id);
+    console.log(chat);
+    res.redirect("/chats");
+});
 
 
 
